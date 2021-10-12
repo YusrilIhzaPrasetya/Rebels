3460cf0a9dbe65dccf4ee071cc652e33d00adf7c
YusrilIhzaPrasetya <yusrilihzaprasetya@gmail.com>
2021-10-12 14:38:20 +0700

    Menambahkan 1 buah modal untuk menangani update data

--- client/src/components/DashboardCard.jsx
+++ client/src/components/DashboardCard.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React,{useState} from 'react'
+import ModalUpdate from './ModalUpdate'
 
 function DashboardCard() {
+
+    const [openModal, setOpenModal] = useState(false)
+
     return (
         <div className="pt-20 flex mx-32">
             <div className="flex flex-row mr-8 justify-between w-screen py-3 px-8 rounded-lg bg-gray-400">
@@ -8,7 +12,10 @@ function DashboardCard() {
                 <p>Tanggal Waktu</p>
                 <p>Nominal</p>
             </div>
-                <button className="border-transparent bg-green-500 text-white text-sm py-3 px-5 rounded-lg">=</button>
+                <button onClick={()=>{
+                    setOpenModal(true)
+                }} className="border-transparent bg-green-500 text-white text-sm py-3 px-5 rounded-lg">=</button>
+                {openModal && <ModalUpdate closeModal={setOpenModal} />}
         </div>
     )
 }
