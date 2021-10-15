c22868fdd1aa0cc3723609c34ed76e0733c4c593
YusrilIhzaPrasetya <yusrilihzaprasetya@gmail.com>
2021-10-15 19:36:18 +0700

    Commit Checkpoint

--- client/src/components/DashboardCard.jsx
+++ client/src/components/DashboardCard.jsx
@@ -7,10 +7,13 @@ function DashboardCard() {
 
     return (
         <div className="pt-20 flex mx-32">
-            <div className="flex flex-row mr-8 justify-between w-screen py-3 px-8 rounded-lg bg-gray-400">
+            <div className="flex flex-row mr-8 justify-between w-screen py-3 px-8 rounded-lg bg-gray-400 cursor-pointer" onClick={()=>{
+                setOpenModal(true)
+            }}>
                 <p>Jenis Aktifitas</p>
                 <p>Tanggal Waktu</p>
                 <p>Nominal</p>
+                {openModal && <ModalUpdate closeModal={setOpenModal} />}
             </div>
                 <button onClick={()=>{
                     setOpenModal(true)
